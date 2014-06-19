1d59352583c4e6e7b0c90c8d0061f8d291a8131b
RobinRaawr <Rkok94@gmail.com>
2014-06-19 19:38:48 +0200

    Allereerste commit (:

---
+++ Vraag en Aanbod/src/app/Overviewframe.java
@@ -0,0 +1,11 @@
+package app;
+
+import javax.swing.JFrame;
+
+public class Overviewframe extends JFrame {
+	
+	public Overviewframe(){
+		
+		setSize(360, 576);
+	}
+}
---
+++ Vraag en Aanbod/src/app/Overviewpanel.java
@@ -0,0 +1,15 @@
+package app;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class Overviewpanel {
+	
+	public Overviewpanel(){		
+		
+		JPanel panel = new JPanel();
+		
+		JLabel lbltest = new JLabel("TESTTESTTEST");
+		panel.add(lbltest);
+	}
+}
