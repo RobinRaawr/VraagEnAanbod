d13728da8dc49eba332a1a920a2e5cd74ced1b5f
RobinRaawr <Rkok94@gmail.com>
2014-06-19 20:28:07 +0200

    Frame and Panel connected YIS

--- Vraag en Aanbod/src/app/Overviewframe.java
+++ Vraag en Aanbod/src/app/Overviewframe.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class Overviewframe extends JFrame {
 	
-	private Overviewpanel overviewpanel;
+	private Overviewpanel overviewpanel = new Overviewpanel();	
 	
 	public Overviewframe(){
 		super("Vraag en Aanbod");
@@ -12,12 +12,11 @@ public class Overviewframe extends JFrame {
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Zorg dat het kruisje daadwerkelijk de app sluit.
 		this.setSize(360, 576); // Eerst de size dan pas locatie zetten anders nope
-		this.setLocationRelativeTo(null); // Zorgt dat de app in het midden begint en niet links boven
+		this.setLocationRelativeTo(null); // Zorgt dat de app in het midden begint en niet links boven			
+		this.setContentPane(overviewpanel);
+		this.revalidate(); // JFrame's refresh methode	
 		
-		this.revalidate(); // JFrame's refresh methode
 		
-		
-		//setContentPane(overviewpanel);
 		System.out.println("Overviewframe is gemaakt fgt");
 	}
 }
