5ea5c59fc7dc2f21b9ba947675a50ad7e3f69f63
RobinRaawr <Rkok94@gmail.com>
2014-06-19 20:10:01 +0200

    Werkend frame zonder panel

--- Vraag en Aanbod/src/app/Overviewframe.java
+++ Vraag en Aanbod/src/app/Overviewframe.java
@@ -4,8 +4,20 @@ import javax.swing.JFrame;
 
 public class Overviewframe extends JFrame {
 	
+	private Overviewpanel overviewpanel;
+	
 	public Overviewframe(){
+		super("Vraag en Aanbod");
+	
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Zorg dat het kruisje daadwerkelijk de app sluit.
+		this.setSize(360, 576); // Eerst de size dan pas locatie zetten anders nope
+		this.setLocationRelativeTo(null); // Zorgt dat de app in het midden begint en niet links boven
 		
-		setSize(360, 576);
+		this.revalidate(); // JFrame's refresh methode
+		
+		
+		//setContentPane(overviewpanel);
+		System.out.println("Overviewframe is gemaakt fgt");
 	}
 }
