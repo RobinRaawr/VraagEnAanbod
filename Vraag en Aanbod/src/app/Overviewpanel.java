b4af5edb811b390395fcc3f058abf2c090d2b1b1
RobinRaawr <Rkok94@gmail.com>
2014-06-19 20:42:38 +0200

    Vraag en aanbod only visual beginnetje

--- Vraag en Aanbod/src/app/Overviewpanel.java
+++ Vraag en Aanbod/src/app/Overviewpanel.java
@@ -2,14 +2,31 @@ package app;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import net.miginfocom.swing.MigLayout;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
-public class Overviewpanel {
+public class Overviewpanel extends JPanel {
 	
-	public Overviewpanel(){		
+	public Overviewpanel(){
+		setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
+		JPanel UpperPanel = new JPanel();
+		UpperPanel.setBackground(Color.GRAY);
+		add(UpperPanel, BorderLayout.NORTH);
+		UpperPanel.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lbltest = new JLabel("TESTTESTTEST");
-		panel.add(lbltest);
+		JLabel UpperLabel = new JLabel("  Vraag en Aanbod");
+		UpperLabel.setForeground(Color.WHITE);
+		UpperLabel.setFont(new Font("Segoe UI Light", Font.PLAIN, 32));
+		UpperPanel.add(UpperLabel, BorderLayout.SOUTH);
+		
+		JPanel LowerPanel = new JPanel();
+		LowerPanel.setBackground(Color.GRAY);
+		add(LowerPanel, BorderLayout.SOUTH);
 	}
 }
