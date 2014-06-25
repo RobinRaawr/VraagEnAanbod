e7ed620452c3732658eae12ff6ef26cbe9296029
RobinRaawr <Rkok94@gmail.com>
2014-06-25 21:36:01 +0200

    Beginnetje met de Fake Database

---
+++ Vraag en Aanbod/src/app/FakeDatabase.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.util.ArrayList;
+
+public class FakeDatabase {
+
+	public ArrayList<User> userList = new ArrayList<User>();
+	public ArrayList<Event> eventList = new ArrayList<Event>();
+	
+}
